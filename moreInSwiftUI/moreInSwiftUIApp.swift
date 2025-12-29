ef3aafa3d45840b1992e3a8bcc47c1f59829c68b
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2025-12-29 13:02:04 +0500

    Initial Commit

---
+++ moreInSwiftUI/moreInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  moreInSwiftUIApp.swift
+//  moreInSwiftUI
+//
+//  Created by Mac mini on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct moreInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moreInSwiftUITests/moreInSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  moreInSwiftUITests.swift
+//  moreInSwiftUITests
+//
+//  Created by Mac mini on 29/12/2025.
+//
+
+import Testing
+@testable import moreInSwiftUI
+
+struct moreInSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
